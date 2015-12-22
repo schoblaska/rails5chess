77edc97f5cb903c989e25e574da5a82d37281233
Joey Schoblaska <joey.schoblaska@gmail.com>
2015-12-22 15:49:26 -0600

    identify connections by a random uuid

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,5 +1,10 @@
 # Be sure to restart your server when you modify this file. Action Cable runs in an EventMachine loop that does not support auto reloading.
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :uuid
+
+    def connect
+      self.uuid = SecureRandom.hex
+    end
   end
 end
