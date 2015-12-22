edea67e16dc46c8c399a5e785f4e2c52776a000d
Joey Schoblaska <joey.schoblaska@gmail.com>
2015-12-22 15:44:56 -0600

    enable websocket route and client bootstrap

--- app/assets/javascripts/cable.coffee
+++ app/assets/javascripts/cable.coffee
@@ -6,6 +6,6 @@
 #= require action_cable
 #= require_self
 #= require_tree ./channels
-#
-# @App ||= {}
-# App.cable = ActionCable.createConsumer()
+
+@App ||= {}
+App.cable = ActionCable.createConsumer()
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
   root to: "welcome#index"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  # Serve websocket cable requests in-process
-  # mount ActionCable.server => '/cable'
+  mount ActionCable.server => "/cable"
 end
