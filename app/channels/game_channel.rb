5e8fe72cb73ed9ceb8263d01078851a54004309d
Joey Schoblaska <joey.schoblaska@gmail.com>
2015-12-24 14:34:47 -0600

    find game or create seek when subbing to game channel

--- app/channels/game_channel.rb
+++ app/channels/game_channel.rb
@@ -1,10 +1,28 @@
 # Be sure to restart your server when you modify this file. Action Cable runs in an EventMachine loop that does not support auto reloading.
 class GameChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
+    stream_from "player_#{uuid}"
+    find_game
   end
 
   def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
+    redis.srem("seeks", uuid)
+    # forfeit game
+  end
+
+  private
+
+  def find_game
+    if opponent = redis.spop("seeks")
+      white, black = [uuid, opponent].shuffle
+      ActionCable.server.broadcast "player_#{white}", "You play white"
+      ActionCable.server.broadcast "player_#{black}", "You play black"
+    else
+      redis.sadd("seeks", uuid)
+    end
+  end
+
+  def redis
+    @redis ||= Redis.new(ActionCable.server.config.redis)
   end
 end
